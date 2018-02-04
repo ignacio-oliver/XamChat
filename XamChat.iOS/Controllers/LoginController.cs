c73e58c05e30f031899db049bf1f4d281d15c75b
Ignacio Oliver <ignacio.oliver@gmail.com>
2018-02-03 22:42:59 -0300

    Module 1 - Chapter 5 - iOS UI - Started

---
+++ XamChat.iOS/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace XamChat.iOS
+{
+    public partial class LoginController : UIViewController
+    {
+        public LoginController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
